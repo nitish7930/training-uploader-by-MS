f5d0c1ea7f264b3926bf27a4c544c0fc6dc1eb28
AllenExtractor <dayarmustfa@gmail.com>
2026-06-10 14:01:04 +0530

    𝐓𝐞𝐚𝐦 𝐂𝐢𝐧𝐝𝐞𝐫𝐞𝐥𝐥𝐚

--- modules/main.py
+++ modules/main.py
@@ -260,10 +260,10 @@ async def txt_handler(bot: Client, m: Message):
              #url = f"https://player.muftukmall.site/?id={id}"
             #elif '/master.mpd' in url:
              #id =  url.split("/")[-2]
-             #url = f"https://anonymouspwplayerrr-31d6706c7a3b.herokuapp.com/pw?url={url}?token={raw_text4}"
+             #url = f"{PWAPI2}?url={url}?token={raw_text4}"
             #url = f"https://madxapi-d0cbf6ac738c.herokuapp.com/{id}/master.m3u8?token={raw_text4}"
             elif "d1d34p8vz63oiq" in url or "sec1.pw.live" in url:
-             url = f"https://anonymouspwplayerrr-31d6706c7a3b.herokuapp.com/pw?url={url}&token={raw_text4}"
+             url = f"{PWAPI2}?url={url}&token={raw_text4}}"
                      
                                                          
             name1 = links[i][0].replace("\t", "").replace(":", "").replace("/", "").replace("+", "").replace("#", "").replace("|", "").replace("@", "").replace("*", "").replace(".", "").replace("https", "").replace("http", "").strip()
@@ -497,10 +497,10 @@ async def txt_handler(bot: Client, m: Message):
              #url = f"https://player.muftukmall.site/?id={id}"
             #elif '/master.mpd' in url:
              #id =  url.split("/")[-2]
-             #url = f"https://anonymouspwplayerrr-31d6706c7a3b.herokuapp.com/pw?url={url}?token={raw_text4}"
+             #url = f"{PWAPI2}?url={url}?token={raw_text4}"
             #url = f"https://madxapi-d0cbf6ac738c.herokuapp.com/{id}/master.m3u8?token={raw_text4}"
             elif"d1d34p8vz63oiq" in url or "sec1.pw.live" in url:
-             url = f"https://anonymouspwplayerrr-31d6706c7a3b.herokuapp.com/pw?url={url}&token={raw_text4}"
+             url = f"{PWAPI2}?url={url}&token={raw_text4}"
                 
            
             elif "apps-s3-jw-prod.utkarshapp.com" in url:
@@ -635,7 +635,7 @@ def register_owner_commands(bot):
         db.register_user(msg.from_user.id)
         owner_text = (
             "┌──────────────────────────┐\n"
-            "**💥Contact**: @CinderellaContactBot\n"
+            "**▪️Owner id Dead. ▪️**\n"
             "└──────────────────────────┘\n\n"
         )
         await msg.reply_text(owner_text)
